08b849c224b91c6e13c3dd05cfa9239bb80655f4
Guernion Sylvain <gsylvain35@gmail.com>
2013-12-07 23:24:49 +0100

    disable acra

--- src/org/ydle/Ydle.java
+++ src/org/ydle/Ydle.java
@@ -6,14 +6,14 @@ import org.acra.annotation.ReportsCrashes;
 import android.app.Application;
 
 
-@ReportsCrashes(
-		formKey = "",
-        formUri = "https://sguernion.cloudant.com/acra-storage/_design/acra-storage/_update/report",
-        reportType = org.acra.sender.HttpSender.Type.JSON,
-        httpMethod = org.acra.sender.HttpSender.Method.PUT,
-        formUriBasicAuthLogin="bothastrastandishoustarr",
-        formUriBasicAuthPassword="I2eIcIgDFlveY6qoY3slfnTO"
-)
+//@ReportsCrashes(
+//		formKey = "",
+//        formUri = "https://sguernion.cloudant.com/acra-storage/_design/acra-storage/_update/report",
+//        reportType = org.acra.sender.HttpSender.Type.JSON,
+//        httpMethod = org.acra.sender.HttpSender.Method.PUT,
+//        formUriBasicAuthLogin="bothastrastandishoustarr",
+//        formUriBasicAuthPassword="I2eIcIgDFlveY6qoY3slfnTO"
+//)
 public class Ydle extends Application {
 
 	@Override
